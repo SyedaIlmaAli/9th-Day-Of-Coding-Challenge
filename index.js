aa41bf0f4221cd3d8f267efc58d767ff0e78958c
SyedaIlmaAli <160448031+SyedaIlmaAli@users.noreply.github.com>
2024-05-27 03:25:43 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+"use strict";
+//Day 09
+// Question 25 and 26
+let alienColor = "Green";
+if (alienColor === "Green") {
+    console.log("You just earned 10 points.");
+}
+else {
+    console.log("You just earned 5 points.");
+}
+//Question 27
+let alienColor1 = "Green";
+if (alienColor1 === "Green") {
+    console.log("You just earned 10 points.");
+}
+else if (alienColor1 === "Yellow") {
+    console.log("You just earned 10 points.");
+}
+else {
+    console.log("You just earned 10 points.");
+}
---
+++ index.ts
@@ -0,0 +1,24 @@
+//Day 09
+
+// Question 25 and 26
+
+let alienColor: string = "Green";
+
+if (alienColor === "Green") {
+  console.log("You just earned 10 points.");
+} else {
+  console.log("You just earned 5 points.");
+}
+
+//Question 27
+
+let alienColor1: string = "Green";
+
+if (alienColor1 === "Green") {
+  console.log("You just earned 10 points.");
+} else if (alienColor1 === "Yellow") {
+  console.log("You just earned 10 points.");
+} else {
+  console.log("You just earned 10 points.");
+}
+
